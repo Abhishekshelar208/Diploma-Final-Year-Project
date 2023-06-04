b4edbdb47602ce32e714881178ccd10b37911196
Abhishek Dadasaheb Shelar <99247575+Abhishekshelar208@users.noreply.github.com>
2023-06-04 03:37:04 -0700

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,15 @@
+from sys import path
+import os
+
+class Dataset:
+    def open_img(self):
+        os.startfile(r"C:\Users\User\Desktop\FACE-ATTENDACE-SYSTEM\data_img")
+    
+    
+
+
+
+
+
+obj=Dataset()
+obj.open_img()
